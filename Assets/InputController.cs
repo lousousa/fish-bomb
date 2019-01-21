87946981f29c822a145e06f76906d47337190fdd
louizard <rumpertumpski@gmail.com>
2019-01-21 17:02:11 -0300

    add input controller

---
+++ Assets/InputController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    bool fireBlocked = false;
+
+    void Update () {
+
+        if (Input.GetKey("up")) {
+
+            GameController.playerDirection = Vector2.up;
+
+        } else if (Input.GetKey("right")) {
+
+            GameController.playerDirection = Vector2.right;
+
+        } else if (Input.GetKey("down")) {
+
+            GameController.playerDirection = Vector2.down;
+
+        } else if (Input.GetKey("left")) {
+
+            GameController.playerDirection = Vector2.left;
+
+        }
+
+        if (Input.GetButton("Fire3")) {
+            if (!fireBlocked) {
+                Invoke("UnblockFire", .75f);
+                fireBlocked = true;
+                GameController.playerFire = true;
+            }
+        }
+
+    }
+
+    private void UnblockFire() {
+        fireBlocked = false;
+    }
+
+}
