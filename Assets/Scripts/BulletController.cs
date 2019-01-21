4678aee0978722440f992a60438e951906010dcf
louizard <rumpertumpski@gmail.com>
2019-01-21 17:45:06 -0300

    re organize project

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    Rigidbody2D rgdb;
+    Vector2 direction;
+    int bulletType; // 0 = player; 1 = enemy;
+
+    private void Start() {
+        rgdb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        Vector2 pos = transform.position;
+        float screenLimits = 20;
+        if (GameController.playerWasDefeated || GameController.enemiesActive.Count == 0) {
+            rgdb.velocity = Vector2.zero;
+        } else {
+            rgdb.velocity = pos.x < screenLimits && pos.x > -screenLimits && pos.y < screenLimits && pos.y > -screenLimits ?
+                direction * 4f : Vector2.zero;
+        }
+    }
+
+    public void Resume(Vector2 dir, Vector2 pos, int type) {
+        bulletType = type;
+        direction = dir;
+        transform.position = pos;
+        transform.parent = null;
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (bulletType == 0 && collision.CompareTag("Enemy")) {
+            EnemyController ctrl = collision.gameObject.GetComponent<EnemyController>();
+            if (ctrl.IsActive()) {
+                ctrl.Deactivate();
+                Camera.main.GetComponent<CameraShake>().shakeDuration = .2f;
+            }
+        } else if (bulletType == 1 && collision.CompareTag("Player")) {
+            GameController.playerWasDefeated = true;
+            Camera.main.GetComponent<CameraShake>().shakeDuration = .2f;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameController {
+
+    public static Vector2 playerDirection = Vector2.zero;
+    public static bool playerFire = false;
+    public static bool playerWasDefeated = false;
+    public static Dictionary<int, bool> enemiesActive = new Dictionary<int, bool>();
+    public static Dictionary<Vector2, Quaternion> directionsRotations = new Dictionary<Vector2, Quaternion> {
+        { Vector2.zero, Quaternion.Euler(0, 0, 0) },
+        { Vector2.up, Quaternion.Euler(0, 0, 0) },
+        { Vector2.right, Quaternion.Euler(0, 0, -90) },
+        { Vector2.down, Quaternion.Euler(0, 0, 180) },
+        { Vector2.left, Quaternion.Euler(0, 0, 90) }
+    };
+
+}
