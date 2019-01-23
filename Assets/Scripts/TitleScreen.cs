8c031a54f2315f1d9f738d87d0828f13ed13e8a9
louizard <rumpertumpski@gmail.com>
2019-01-23 16:59:46 -0300

    add text to title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+
+    Text text;
+
+	void Start () {
+        text = GetComponentInChildren<Text>();
+        FlashText();
+    }
+
+    bool textEnabled = true;
+    void FlashText() {
+        text.enabled = !text.enabled;
+        Invoke("FlashText", .5f);
+    }
+}
